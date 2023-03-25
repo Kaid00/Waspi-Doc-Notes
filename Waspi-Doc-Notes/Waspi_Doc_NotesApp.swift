6aefe6a4730eb5fdc314754ff77c304e2ae7cf28
Azamah Jr <depixellife@gmail.com>
2023-03-25 21:28:15 +0100

    Initial Commit

---
+++ Waspi-Doc-Notes/Waspi_Doc_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Waspi_Doc_NotesApp.swift
+//  Waspi-Doc-Notes
+//
+//  Created by Azamah Junior Khan on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Waspi_Doc_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
